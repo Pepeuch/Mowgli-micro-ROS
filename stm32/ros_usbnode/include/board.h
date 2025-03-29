52a3c715c203983f41a5f356797c86129ec38b10
Pepeuch <pepeuch@gmail.com>
2025-03-29 12:58:10 +0100

    modified:   stm32/ros_usbnode/include/board.h

--- stm32/ros_usbnode/include/board.h
+++ stm32/ros_usbnode/include/board.h
@@ -177,11 +177,10 @@ extern uint8_t EMERGENCY_DEBUG;// Enable Emergency debugging
 #define WHEEL_LIFT_RED_PORT GPIOD
 
 /* Play button - (LOW when pressed) */
-#ifndef PLAY_BUTTON_PIN
-#if BOARD_YARDFORCE500_VARIANT_B
-#define PLAY_BUTTON_PIN GPIO_PIN_9
+#ifdef BOARD_YARDFORCE500_VARIANT_B
+        #define PLAY_BUTTON_PIN GPIO_PIN_9
 #else
-#define PLAY_BUTTON_PIN GPIO_PIN_7
+        #define PLAY_BUTTON_PIN GPIO_PIN_7
 #endif
 #define PLAY_BUTTON_PORT GPIOC
 #define PLAY_BUTTON_GPIO_CLK_ENABLE() __HAL_RCC_GPIOC_CLK_ENABLE()
